44bc0a45284a3a65be118635a57c30424865ad53
rashel68 <rashel341068@gmail.com>
2021-07-31 21:14:24 +0600

    first js

---
+++ main.js
@@ -0,0 +1,67 @@
+var onionPrice = '22.5';
+var ricePrice = 50.60;
+
+// var onion = parseFloat(onionPrice);
+// console.log(onion + ricePrice);
+
+var mangoPrice = '55.36';
+var eggPrice = '10.5';
+
+var mango = parseFloat(mangoPrice);
+var egg = parseFloat(eggPrice);
+
+// console.log(mango + egg);
+
+var penPrice ='10';
+var khataPrice = '25';
+// console.log(penPrice + khataPrice);
+var pen = parseInt(penPrice);
+var khata = parseInt(khataPrice);
+// console.log(pen + khata);
+
+var mousePrice = '44.5';
+var laptopPrice = '445.65';
+var mouse = parseFloat(mousePrice);
+var laptop = parseFloat(laptopPrice);
+// console.log(mousePrice + laptopPrice);
+// console.log(mouse + laptop);
+
+var potatoPrice = 40;
+var userName = 'Rashel';
+var isRaining = true;
+
+// console.log(typeof potatoPrice);
+// console.log(typeof userName);
+// console.log(typeof isRaining);
+
+// mathmatical operation 
+var number1 = 10;
+var number2 = 3;
+var number3 = 2;
+var number4 = 8;
+var number5 = 9;
+
+var numberSumation = number1 + number2 + number3;
+var numberDifference = number1 - number2;
+var numberMultiplication = number1 * number3;
+var numberExponentiation = number1 ** number2;
+var numberDivision = number1 / number3;
+var numberIncrement = ++number4;
+var numberDecrement = --number5;
+
+console.log(numberSumation);
+console.log(numberDifference);
+console.log(numberMultiplication);
+console.log(numberExponentiation);
+console.log(numberDivision);
+console.log(numberIncrement);
+console.log(numberDecrement);
+
+// special adition 
+var firstNumber = 0.4;
+var secondNumber = 0.69;
+var toalNumber = firstNumber + secondNumber;
+// console.log(toalNumber.toFixed(2));
+// console.log(parseFloat(toalNumber.toFixed(2)));
+
+
